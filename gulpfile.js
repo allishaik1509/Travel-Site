0700674666ed6942f4aed04c45b7d9c0c5f6bcab
ali <alliluddinshaik@gmail.com>
2018-12-24 23:14:07 +1100

    Added postcss-nested

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var gulp 	= require('gulp'),
-	watch 	= require('gulp-watch'),
-	postcss = require('gulp-postcss'),
-	autoprefixer = require('autoprefixer');
+var gulp 			= require('gulp'),
+	watch 			= require('gulp-watch'),
+	postcss 		= require('gulp-postcss'),
+	autoprefixer 	= require('autoprefixer'),
+	cssvars			= require('postcss-simple-vars'),
+	nested			= require('postcss-nested'),
+	batch			= require('gulp-batch');
 
 gulp.task('default',function()
 {
@@ -16,16 +19,16 @@ gulp.task('html',function()
 gulp.task('styles',function()
 {
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([autoprefixer]));
+	.pipe(postcss([cssvars, nested, autoprefixer]))
 	.pipe(gulp.dest('./app/temp/styles'));
 })
 gulp.task('watch',function(){
 
-	gulp.watch('./app/index.html',function(){
+	watch('./app/index.html',batch(function(){
 		gulp.start('html');
-		});
+		}));
 
-	gulp.watch('./app/assets/styles/**/*.css',function(){
+	gulp.watch('./app/assets/styles/**/*.css',batch(function(){
 		gulp.start('styles');
-		});
+		}));
 });
