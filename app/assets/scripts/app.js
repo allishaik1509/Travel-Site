26aa36342499bdd956a1518289ada8b46484f99d
ali <alliluddinshaik@gmail.com>
2019-01-26 23:15:55 +1100

    Added stickey header with jquery scroll to navigate the nav-bar

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 
 var mobileMenu = new MobileMenu();
 var revealOnScroll = new RevealOnScroll();
+var stickyHeader = new StickyHeader();
