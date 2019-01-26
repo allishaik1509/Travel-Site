b2ca4d26df5a50a992f981a195817ed049314021
ali <alliluddinshaik@gmail.com>
2019-01-27 03:14:04 +1100

    completed Modal Overlay of the site

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import modal from './modules/modal';
 
 
 var mobileMenu = new MobileMenu();
 var revealOnScroll = new RevealOnScroll();
 var stickyHeader = new StickyHeader();
+var Modal = new modal();
